d45236a84c791ecca5d3803818336b87e40c09c7
SonMyoungJin <tbvltm0306@naver.com>
2016-10-10 12:15:46 +0900

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,11 @@
+﻿class dice {
+    private int number = 0;
+    public void roll() {
+        number = (int) (Math.random() * 6);
+        System.out.println("Turn on " + number);
+    }
+    public int getNum() {
+        return this.number;
+    }
+}
+
