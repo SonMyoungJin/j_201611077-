2dc6205987e49ed7662b3675bdcae125e5ded738
SonMyoungJin <tbvltm0306@naver.com>
2016-11-09 11:08:44 +0900

    Add files via upload

---
+++ DrawTriangle.java
@@ -0,0 +1,21 @@
+class DrawTriangle {
+  
+  public static void printtri() {
+    String s="*";
+    String e=" ";
+    StringBuffer sbuf=new StringBuffer();
+    for(int i=0;i<6;i++) {
+      for(int j=0;j<(6-i);j++) {
+        sbuf.append(e);
+      }
+      for(int j=0;j<(i*2)-1;j++) {
+        sbuf.append(s);
+      }
+      System.out.printf("%d %s\n",i,sbuf.toString());
+      sbuf.delete(0, sbuf.length());
+    }
+  }
+  public static void main(String[] args) {
+    printtri();
+  }
+}
