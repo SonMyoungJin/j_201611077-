daee0be0efaf58955cea4ea0322758ed59e6299b
SonMyoungJin <tbvltm0306@naver.com>
2016-11-30 11:57:29 +0900

    Delete SeaTurtle.java

--- SeaTurtle.java
+++
@@ -1,7 +0,0 @@
-package com.sd.ch8;
-//같은 패키지라서 생략가능 import com.sd.ch8.Turtle;
-public class SeaTurtle extends Turtle{
-  public void swim() {
-    System.out.print("A sea turtle swims...");
-      }
-}
