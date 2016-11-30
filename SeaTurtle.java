c1cdf66bf2c609cd64052ef81d26c7a7f7a2484c
SonMyoungJin <tbvltm0306@naver.com>
2016-11-30 11:56:28 +0900

    Add files via upload

---
+++ SeaTurtle.java
@@ -0,0 +1,7 @@
+package com.sd.ch8;
+//같은 패키지라서 생략가능 import com.sd.ch8.Turtle;
+public class SeaTurtle extends Turtle{
+  public void swim() {
+    System.out.print("A sea turtle swims...");
+      }
+}
---
+++ Turtle.java
@@ -0,0 +1,11 @@
+package com.sd.ch8;
+public class Turtle {         //파일마다 하나의 클래스가 사용되니까 public해줌
+  private int nLimbs=4;
+  protected String food;
+  public void setFood(String f) {
+    this.food=f;
+  }
+  public void eat() {
+    System.out.print("Turtle eats "+food);
+  }
+}
